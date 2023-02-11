7c02ae3dd112a49056e52bcca485949d69c17888
rangelkohei <kohei_adonai74739@outlook.com>
2023-02-10 23:00:06 -0300

    tabela tarefas pendentes na tela inicial

--- View/home.php
+++ View/home.php
@@ -38,19 +38,26 @@
 								<hr />
 
 								<?php
-								$html = '
-									<div class="row mb-3 d-flex align-items-center tarefa">
+								$query = "SELECT * FROM tarefa WHERE status_tarefa = 0";
+								$stmt = $pdo->prepare($query);
+								$stmt->execute();
+								$consulta = $stmt->fetchAll();
+
+								foreach($consulta as $result){
+									echo '
+										<div class="row mb-3 d-flex align-items-center tarefa">
 										<div class="col-sm-9">
-											<h5>Lavar o carro</h5>
-											<small>lavar com esponja, detergente e agua</small>
+											<h5>'. $result['titulo'] .'</h5>
+											<small>'. $result['descricao'] .'</small>
 										</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger"></i>
 											<i class="fas fa-edit fa-lg text-info"></i>
 											<i class="fas fa-check-square fa-lg text-success"></i>
 										</div>
-									</div>';
-									
+										</div>';
+								}
+
 								?>
 								
 								
